5df8cd1557412bf1fd3addcd7c6c0f7dd90cbb1a
theripper93 <tsplab@gmail.com>
2023-08-02 02:07:02 +0200

    a

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,9 @@
 import {initConfig} from "./config.js";
 import { registerSettings } from "./settings.js";
 
-export const MODULE_ID = "module-id";
+export const MODULE_ID = "quick-grid-align";
 
 Hooks.on("init", () => {
     initConfig();
     registerSettings();
-    console.log("test")
 });
